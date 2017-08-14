291a7022a55e1234b49321677070088451783bc2
admin-m <977986310@qq.com>
2017-08-14 11:13:39 +0800

    加密解密

--- shiro-demo-05/src/main/java/TestPasswordService.java
+++ shiro-demo-05/src/main/java/TestPasswordService.java
@@ -1,5 +1,9 @@
 import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.DefaultHashService;
+import org.apache.shiro.crypto.hash.HashRequest;
 import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.util.SimpleByteSource;
 import org.junit.Test;
 
 public class TestPasswordService extends BaseTest{
@@ -33,5 +37,20 @@ public class TestPasswordService extends BaseTest{
     public void testHashedCredentialsMatcherWithMyRealm2() {
         //使用testGeneratePassword生成的散列密码
         login("classpath:shiro-hashedCredentialsMatcher.ini", "liu", "123");
+    }
+
+    @Test
+    public void testdemo() {
+        DefaultHashService hashService = new DefaultHashService(); //默认算法SHA-512
+        hashService.setHashAlgorithmName("SHA-512");
+        hashService.setPrivateSalt(new SimpleByteSource("123")); //私盐，默认无
+        hashService.setGeneratePublicSalt(true);//是否生成公盐，默认false
+        hashService.setRandomNumberGenerator(new SecureRandomNumberGenerator());//用于生成公盐。默认就这个
+        hashService.setHashIterations(1); //生成Hash值的迭代次数
+
+        HashRequest request = new HashRequest.Builder()
+                .setAlgorithmName("MD5").setSource(ByteSource.Util.bytes("hello"))
+                .setSalt(ByteSource.Util.bytes("123")).setIterations(2).build();
+        String hex = hashService.computeHash(request).toHex();
     }
 }
